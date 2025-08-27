f60e8f8ebc2150078c983cfcaf1d5db3846dc11d
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2025-08-28 04:47:26 +0530

    Update Nabila (1).java

--- Nabila (1).java
+++ Nabila (1).java
@@ -2,16 +2,18 @@ public class Nabila{
       public static void main(String[]args){
 
 int age = 30;
-if(age>18){
-System.out.println("Eiligible for vote");
-}
-else if(age<30){
-System.out.println("Must be voter id");
-}
-else{
-System.out.println("Not eligible for vote");
-}
 
+
+        if(age < 18) {
+            System.out.println("Not eligible for vote");
+        }
+        else if(age < 30) {
+            System.out.println("Must have voter id");
+        }
+        else {
+            System.out.println("Eligible for vote");
+        }
+    
 }
 }
 
